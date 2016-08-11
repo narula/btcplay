c51a9deac2f681f8ba7b88afe8734ffb7a7ca75f
Neha Narula <narula@gmail.com>
2016-08-11 14:43:33 -0400

    x

--- main.go
+++ main.go
@@ -1,12 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/btcutil"
+	"log"
+)
 
 func main() {
-	fmt.Printf("blah\n")
+	chainParams := &chaincfg.TestNet3Params
+
+	privKey, pubKey := btcec.PrivKeyFromBytes(btcec.S256(), []byte("nehanarula123456"))
+	_ = privKey
+	addr, err := btcutil.NewAddressPubKeyHash(btcutil.Hash160(pubKey.SerializeCompressed()), chainParams)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%#v %v %v\n", pubKey, addr, addr.ScriptAddress())
+	// addr: mpYJ4Uj4GRDxt9xNk1Z4FtZBZcYRbhQDQk
+	// txid: e9eb1dc1961a1dfcb0ed16a43efed331dd19e985bf74a5ca16ef8a6c24f09d92
 
 	// TODO
-	// - Make a bitcoin address
-	// - Fund it
-	// - Spend from it
+	// X Make a bitcoin address
+	// X Fund it
+	// - Create a signed transaction spending from it
+	// - Send that transaction to a testnet node
+
+	mytx := wire.NewMsgTx()
+	_ = mytx
+	//	ti := wire.NewTxIn(nil)
+	//	mytx.AddTxIn(ti)
+
+}
+
+func network_crap() {
+	// Use Tadge's testnet server
+	host := "lit3.co:18333"
+	_ = host
+
+	//	conn, err := net.Dial("tcp", host)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+
+	//	n, err := wire.WriteMessageN(conn, msg, 0, chainParams.Net)
+	//	if err != nil {
+	//		panic(err)
+	//	}
 }
