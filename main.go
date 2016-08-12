ef46a5e1253a85337681147bc058586c73fd43cd
Neha Narula <narula@gmail.com>
2016-08-12 10:05:31 -0400

    clean up

--- main.go
+++ main.go
@@ -20,44 +20,40 @@ import (
 // - Send that transaction to a testnet node
 
 func main() {
-	chainParams := &chaincfg.TestNet3Params
-
 	privKey, pubKey := btcec.PrivKeyFromBytes(btcec.S256(), []byte("nehanarula123456"))
-	_ = privKey
+	chainParams := &chaincfg.TestNet3Params
 	addr, err := btcutil.NewAddressPubKeyHash(btcutil.Hash160(pubKey.SerializeCompressed()), chainParams)
+	_ = addr
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%#v %v %v\n", pubKey, addr, addr.ScriptAddress())
 	// addr: mpYJ4Uj4GRDxt9xNk1Z4FtZBZcYRbhQDQk
-	// txid: e9eb1dc1961a1dfcb0ed16a43efed331dd19e985bf74a5ca16ef8a6c24f09d92
-	// pay me script: 76a91462fa0c3c58c7d618994808e7789d7912cc8e40e388ac
+
+	// got bitcoin from testnet faucet
 	txid := "e9eb1dc1961a1dfcb0ed16a43efed331dd19e985bf74a5ca16ef8a6c24f09d92"
+	pay_me := "76a91462fa0c3c58c7d618994808e7789d7912cc8e40e388ac"
+	pay_tadge := "001443aac20a116e09ea4f7914be1c55e4c17aa600b7"
 
 	h, err := chainhash.NewHashFromStr(txid)
 	if err != nil {
 		panic(err)
 	}
-
 	outpoint := wire.NewOutPoint(h, 0)
 	ti := wire.NewTxIn(outpoint, nil)
 	mytx := wire.NewMsgTx()
 	mytx.AddTxIn(ti)
-	b1, err := hex.DecodeString("76a91462fa0c3c58c7d618994808e7789d7912cc8e40e388ac")
 
+	b1, err := hex.DecodeString(pay_me)
 	if err != nil {
 		panic(err)
 	}
-
 	to1 := wire.NewTxOut(1520000000, b1)
-	b2, err := hex.DecodeString("001443aac20a116e09ea4f7914be1c55e4c17aa600b7")
-
+	mytx.AddTxOut(to1)
+	b2, err := hex.DecodeString(pay_tadge)
 	if err != nil {
 		panic(err)
 	}
-
 	to2 := wire.NewTxOut(100000000, b2)
-	mytx.AddTxOut(to1)
 	mytx.AddTxOut(to2)
 	sig, err := txscript.SignatureScript(mytx, 0, b1, txscript.SigHashAll, privKey, true)
 	if err != nil {
@@ -69,7 +65,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%x \n\n%#v\n", buf.Bytes(), mytx)
+	fmt.Printf("%x\n", buf.Bytes())
 }
 
 func network_crap() {
